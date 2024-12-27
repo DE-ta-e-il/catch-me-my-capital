275997c298b34bb9bf984f55a9598a5732c56726
mangodm-web <mangodm.web3@gmail.com>
2024-12-27 16:29:50 +0900

    chore: TDET-51 Add constants to improve maintainability

---
+++ dags/constants.py
@@ -0,0 +1,17 @@
+class Interval:
+    DAILY = "daily"
+    MONTHLY = "monthly"
+
+
+class Owner:
+    DONGWON = "tunacome@gmail.com"
+    DAMI = "mangodm.web3@gmail.com"
+    JUNGMIN = "eumjungmin1@gmail.com"
+    MINHYEOK = "tlsfk48@gmail.com"
+
+
+class Layer:
+    BRONZE = "bronze"
+    SILVER = "silver"
+    GOLD = "gold"
+    LANDING = "landing"
