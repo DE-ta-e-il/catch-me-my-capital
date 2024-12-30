899fdd8b7ea7cd0f4c483342b30006d2eafb5776
j-eum <eumjungmin1@gmail.com>
2024-12-30 17:13:39 +0900

    fix: TDET-59 Address review comments

---
+++ dags/brz_kr_etf_daily/kr_etf_main.py
@@ -0,0 +1,88 @@
+import json
+from datetime import datetime, timedelta
+
+import requests
+from airflow import DAG
+from airflow.exceptions import AirflowFailException
+from airflow.models import Variable
+from airflow.operators.python import PythonOperator, ShortCircuitOperator
+from common.s3_utils import upload_string_to_s3
+
+from brz_kr_etf_daily.kr_etf_utils import (
+    generate_json_s3_key,
+    is_kr_market_open_today,
+)
+
+
+def verify_market_open(ds_nodash):
+    date = datetime.strptime(ds_nodash, "%Y%m%d")
+    return is_kr_market_open_today(date)
+
+
+def fetch_etf_from_krx_api_to_s3(ds_nodash, ds):
+    url = "http://apis.data.go.kr/1160100/service/GetSecuritiesProductInfoService/getETFPriceInfo"
+    params = {
+        "serviceKey": Variable.get("data_kr_service_key"),
+        "numOfRows": 1000,
+        "pageNo": 1,
+        "resultType": "json",
+        "basDt": ds_nodash,
+    }
+
+    all_items = []
+
+    while True:
+        response = requests.get(url, params=params)
+
+        if response.status_code != 200:
+            raise AirflowFailException(
+                f"Failed to fetch API: {response.status_code}, {response.text}"
+            )
+
+        data = response.json().get("response", {}).get("body", {})
+
+        total_count = int(data.get("totalCount"))
+        current_page = int(data.get("pageNo"))
+
+        if total_count == 0:
+            raise Exception(f"No data available: 'totalCount' is 0. Full data: {data}")
+
+        items = data.get("items", {}).get("item", [])
+        all_items.extend(items)
+
+        if len(all_items) >= total_count:
+            break
+
+        params["pageNo"] = current_page + 1
+
+    data_str = json.dumps(all_items)
+    s3_key = generate_json_s3_key(ds)
+
+    upload_string_to_s3(data_str, s3_key)
+
+
+default_args = {
+    "owner": "j-eum",  # TODO: 공통 ENUM적용 예정
+    "retries": 5,
+    "retry_delay": timedelta(hours=1),
+}
+
+with DAG(
+    dag_id="brz_kr_etf_daily",
+    default_args=default_args,
+    description="한국거래소 ETF 종목별 시세",
+    tags=["bronze", "ETF", "daily", "weekday"],
+    schedule="0 0 * * 1-5",
+    start_date=datetime(2020, 1, 1),
+    catchup=False,
+) as dag:
+    verify_market_open = ShortCircuitOperator(
+        task_id="verify_market_open", python_callable=verify_market_open
+    )
+
+    fetch_etf_from_krx_api_to_s3 = PythonOperator(
+        task_id="fetch_etf_krx_api",
+        python_callable=fetch_etf_from_krx_api_to_s3,
+    )
+
+    verify_market_open >> fetch_etf_from_krx_api_to_s3
---
+++ dags/brz_kr_etf_daily/kr_etf_main_deprecated.py
@@ -0,0 +1,77 @@
+import json
+from datetime import datetime
+
+import requests
+from airflow import DAG
+from airflow.operators.python import PythonOperator, ShortCircuitOperator
+from common.s3_utils import upload_string_to_s3
+
+from brz_kr_etf_daily.kr_etf_utils import (
+    generate_json_s3_key,
+    is_kr_market_open_today,
+)
+
+
+def verify_market_open(ds_nodash):
+    date = datetime.strptime(ds_nodash, "%Y%m%d")
+    return is_kr_market_open_today(date)
+
+
+def fetch_etf_from_krx_web_to_s3(ds_nodash, ds):
+    url = "http://data.krx.co.kr/comm/bldAttendant/getJsonData.cmd"
+    data = {
+        "bld": "dbms/MDC/STAT/standard/MDCSTAT04301",
+        "locale": "ko_KR",
+        "trdDd": ds_nodash,
+        "share": 1,
+        "money": 1,
+        "csvxls_isNo": False,
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.132 Safari/537.36",
+        "Referer": "http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC0201020203",
+    }
+
+    response = requests.post(url, data=data, headers=headers)
+
+    if response.status_code != 200:
+        raise Exception(f"Failed to fetch API: {response.status_code}, {response.text}")
+
+    data = response.json()
+    items = data.get("output")
+
+    if not items:
+        raise Exception(
+            f"Data retrieval failed: 'output' is missing or empty. Full data: {data}"
+        )
+
+    data_str = json.dumps(items)
+    s3_key = generate_json_s3_key(ds)
+
+    upload_string_to_s3(data_str, s3_key)
+
+
+default_args = {
+    "owner": "j-eum",  # TODO: 공통 ENUM적용 예정
+}
+
+with DAG(
+    dag_id="brz_kr_etf_daily_deprecated",
+    default_args=default_args,
+    description="한국거래소 ETF 종목별 시세",
+    tags=["bronze", "ETF", "daily", "weekday"],
+    schedule="0 0 * * 1-5",
+    start_date=datetime(2020, 1, 1),
+    end_date=datetime(2019, 12, 31),
+    catchup=False,
+) as dag:
+    verify_market_open = ShortCircuitOperator(
+        task_id="verify_market_open", python_callable=verify_market_open
+    )
+
+    fetch_etf_from_krx_web_to_s3 = PythonOperator(
+        task_id="fetch_etf_krx_web",
+        python_callable=fetch_etf_from_krx_web_to_s3,
+    )
+
+    verify_market_open >> fetch_etf_from_krx_web_to_s3
