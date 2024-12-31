1eb0603caa8c308555424ef1163b3e5a305967d3
Dee H Seon <123602432+boolYikes@users.noreply.github.com>
2024-12-31 19:14:46 +0900

    fix: TDET-50 Fixed GICS crawl output logic. Conformed names to recent convetion changes

---
+++ delete.sh
@@ -0,0 +1,5 @@
+docker rmi catch-me-my-capital-airflow-init:latest
+docker rmi catch-me-my-capital-airflow-triggerer:latest
+docker rmi catch-me-my-capital-airflow-worker:latest
+docker rmi catch-me-my-capital-airflow-webserver:latest
+docker rmi catch-me-my-capital-airflow-scheduler:latest
