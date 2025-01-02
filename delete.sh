d24cbed4ba053c98db148885deec26b031961bc4
Dee H Seon <123602432+boolYikes@users.noreply.github.com>
2025-01-02 09:59:14 +0900

    chore: TDET-50 Changed the name and id of the DAG accordingly

--- delete.sh
+++
@@ -1,5 +0,0 @@
-docker rmi catch-me-my-capital-airflow-init:latest
-docker rmi catch-me-my-capital-airflow-triggerer:latest
-docker rmi catch-me-my-capital-airflow-worker:latest
-docker rmi catch-me-my-capital-airflow-webserver:latest
-docker rmi catch-me-my-capital-airflow-scheduler:latest
