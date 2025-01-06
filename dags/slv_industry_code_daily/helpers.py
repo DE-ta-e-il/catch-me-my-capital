5a9d26b9cb310ceb873865ee62967832508cc7e6
Dee H Seon <123602432+boolYikes@users.noreply.github.com>
2025-01-06 10:52:13 +0900

    chore: TDET-67 Deleted silver DAGs and script

--- dags/slv_industry_code_daily/helpers.py
+++
@@ -1,5 +0,0 @@
-def to_crawl_or_not_to_crawl(must_crawl: bool, crawl_task: str, placeholder_task: str):
-    """
-    Pass task ids as arguments!
-    """
-    return crawl_task if must_crawl else placeholder_task
