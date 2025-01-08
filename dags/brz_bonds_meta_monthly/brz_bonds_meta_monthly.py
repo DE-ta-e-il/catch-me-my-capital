ac2cc457def8d24512d4705ea756670d0824e787
Dee H Seon <123602432+boolYikes@users.noreply.github.com>
2025-01-08 13:53:47 +0900

    chore: TDET-101 In the middle of doing chores

--- dags/brz_bonds_meta_monthly/brz_bonds_meta_monthly.py
+++ dags/brz_bonds_meta_monthly/brz_bonds_meta_monthly.py
@@ -9,10 +9,7 @@ from airflow.utils.task_group import TaskGroup
 from common.constants import Owner
 
 from brz_bonds_meta_monthly.constants import AirflowParam
-from brz_bonds_meta_monthly.extractors import (
-    get_categories,
-    get_metadata,
-)
+from brz_bonds_meta_monthly.extractors import get_metadata
 
 with DAG(
     dag_id="brz_bonds_meta_monthly",
@@ -28,22 +25,19 @@ with DAG(
     tags=["bronze", "bonds metadata", "monthly"],
     description="All Bonds Metadata, States And Corps",
 ) as dag:
-    start_marker = EmptyOperator(task_id="start_of_bronze_bonds_tasks")
+    start_marker = EmptyOperator(task_id="start_of_brz_bonds_meta_tasks")
 
     # Dynamically generate crawling tasks
     with TaskGroup(group_id="crawler_group") as meta_data_crawler_group:
-        # Put prev_task right below category loop to parallelize
-        urls_dict = get_categories()
-        for category in urls_dict:
-            crawl_for_bonds_metadata = PythonOperator(
-                task_id=f"metadata_for_{category}",
-                python_callable=get_metadata,
-                op_args=[category],
-            )
-            crawl_for_bonds_metadata
+        # Loops inside the task now
+        crawl_for_bonds_metadata = PythonOperator(
+            task_id=f"metadata_for_bonds",
+            python_callable=get_metadata,
+        )
+        crawl_for_bonds_metadata
 
     completion_marker = EmptyOperator(
         task_id="bonds_meta_all_success_check",
     )
 
-    meta_data_crawler_group >> completion_marker
+    start_marker >> meta_data_crawler_group >> completion_marker
