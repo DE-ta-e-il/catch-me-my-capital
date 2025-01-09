be97ec7f6e46126888a1952b068bf700a71d2742
JeongMinHyeok <tlsfk48@gmail.com>
2025-01-09 12:28:27 +0900

    feat: TDET-89 A DAG and scripts to news data silver layer feat: A DAG and scripts to news data silver layer & added jdbc secrets & some modified codes added jdbc secrets feat: A DAG and scripts to news data silver layer & added jdbc secrets & some modified codes some modified codes

--- glue_job_scripts/news_glue_job.py
+++ glue_job_scripts/news_glue_job.py
@@ -8,7 +8,6 @@ import boto3
 from awsglue.context import GlueContext
 from awsglue.dynamicframe import DynamicFrame
 from awsglue.job import Job
-from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from botocore.exceptions import ClientError
 from pyspark.context import SparkContext
@@ -19,8 +18,9 @@ from pyspark.sql.functions import (
     regexp_extract,
     to_date,
 )
-from pyspark.sql.types import DateType, DecimalType, StringType
+from pyspark.sql.types import DateType, TimestampType
 
+# Glue Job 초기화
 logging.basicConfig(level=logging.INFO)
 
 args = getResolvedOptions(sys.argv + ["--JOB_NAME", "default_job_name"], ["JOB_NAME"])
@@ -55,7 +55,7 @@ secrets = get_secret()
 # Redshift에서 테이블 존재 여부 확인
 def check_table_exists():
     jdbc_url = secrets[2]
-    query = "SELECT 1 FROM pg_tables WHERE tablename = 'fact_msci_index' AND schemaname = 'silver'"
+    query = "SELECT 1 FROM pg_tables WHERE tablename = 'fact_news' AND schemaname = 'silver'"
 
     try:
         result_df = (
@@ -75,11 +75,10 @@ def check_table_exists():
 # Redshift에서 가장 최근 날짜 가져오기
 def get_latest_date_from_redshift():
     if not check_table_exists():
-        logging.info("테이블이 존재하지 않음. 기본 날짜 사용")
         return datetime.strptime("2015-01-01", "%Y-%m-%d").date()
 
     jdbc_url = secrets[2]
-    query = "(SELECT MAX(date) AS max_date FROM silver.fact_msci_index)"
+    query = "(SELECT MAX(date) AS max_date FROM silver.fact_news)"
 
     try:
         redshift_df = (
@@ -126,7 +125,7 @@ latest_date = get_latest_date_from_redshift()
 
 # S3에서 특정 날짜 이후의 경로 가져오기
 bucket_name = "team3-1-s3"
-prefix = "bronze/msci_index/"
+prefix = "bronze/news/"
 s3_paths = get_s3_paths_after_date(bucket_name, prefix, latest_date)
 
 # S3 경로가 비어 있을 경우 Job 종료
@@ -134,12 +133,9 @@ if not s3_paths:
     logging.info("새로운 데이터가 없음. 작업 종료")
     os._exit(0)
 
+
 # S3에서 데이터 로드
-bronze_df = (
-    spark.read.option("multiline", "true")
-    .option("recursiveFileLookup", "true")
-    .json(s3_paths)
-)
+bronze_df = spark.read.option("multiline", "true").json(s3_paths)
 
 # 파일 경로 및 ymd 추가
 bronze_df = bronze_df.withColumn("file_path", input_file_name())
@@ -147,14 +143,20 @@ bronze_df = bronze_df.withColumn(
     "ymd", regexp_extract("file_path", "ymd=([^/]+)", 1)
 ).drop("file_path")
 
-# 필요한 컬럼만 선택
+# 주요 칼럼 선택 및 변환
 silver_df = bronze_df.select(
     to_date(col("ymd"), "yyyy-MM-dd").alias("date").cast(DateType()),
-    col("close").alias("price").cast(DecimalType(18, 5)),
-    col("index_name").cast(StringType()),
+    col("abstract").alias("abstract"),
+    col("web_url").alias("web_url"),
+    col("headline").alias("headline"),
+    col("pub_date").alias("pub_date").cast(TimestampType()),
+    col("section_name").alias("section_name"),
+    col("byline").alias("byline"),
+    col("word_count").alias("word_count"),
+    col("keywords").alias("keywords"),  # 키워드 리스트만 추출
 )
 
-# create_at, update_at 컬럼 추가
+# 메타데이터 컬럼 추가 (생성 및 업데이트 시간)
 silver_df = silver_df.withColumn("create_at", current_timestamp())
 silver_df = silver_df.withColumn("update_at", current_timestamp())
 
@@ -163,19 +165,32 @@ silver_dynamic_frame = DynamicFrame.fromDF(
     silver_df, glueContext, "silver_dynamic_frame"
 )
 
-# Redshift에 저장
-WriteToRedshift = glueContext.write_dynamic_frame.from_options(
+# Redshift로 데이터 쓰기
+glueContext.write_dynamic_frame.from_options(
     frame=silver_dynamic_frame,
     connection_type="redshift",
     connection_options={
         "url": secrets[2],
         "user": secrets[0],
         "password": secrets[1],
-        "dbtable": "silver.fact_msci_index",
-        "redshiftTmpDir": "s3://team3-1-s3/data/redshift_temp/",
-        "preactions": "CREATE TABLE IF NOT EXISTS silver.fact_msci_index (date DATE, price DECIMAL(18, 5), index_name VARCHAR, create_at TIMESTAMP, update_at TIMESTAMP);",
+        "dbtable": "silver.fact_news",
+        "redshiftTmpDir": f"s3://{bucket_name}/data/redshift_temp/",
+        "preactions": """
+            CREATE TABLE IF NOT EXISTS silver.fact_news (
+                date DATE,
+                abstract VARCHAR,
+                web_url VARCHAR,
+                headline VARCHAR,
+                pub_date TIMESTAMP,
+                section_name VARCHAR,
+                byline VARCHAR,
+                word_count INTEGER,
+                keywords VARCHAR,
+                create_at TIMESTAMP,
+                update_at TIMESTAMP
+            );
+        """,
     },
-    transformation_ctx="WriteToRedshift",
 )
 
 job.commit()
